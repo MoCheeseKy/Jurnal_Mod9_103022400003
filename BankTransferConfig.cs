ba17b5948d2868a29adfad151d908b6432fb6197
MoCheeseKy <rifky.muhammadprayudhi@gmail.com>
2026-05-02 15:15:36 +0700

    last commit

--- BankTransferConfig.cs
+++ BankTransferConfig.cs
@@ -7,34 +7,6 @@ namespace Modul9_103022400003
 {
     public class BankTransferConfig
     {
-        
-
-        //File json
-        //{
-        //  "lang": "CONFIG1",
-        //  "transfer": {
-        //    "threshold": "CONFIG2",
-        //    "low_fee": "CONFIG3",
-        //    "high_fee": "CONFIG4"
-        //  },
-        //  "methods": "CONFIG5",
-        //  "confirmation": {
-        //    "en": "CONFIG6",
-        //    "id": "CONFIG7"
-        //  }
-        //}
-
-        //1. bila jumlah yang yang di-transfer pada bagian sebelumnya(4-a) kurang dari
-        //atau sama dengan nilai CONFIG2 atau “threshold”, maka biaya transfer adalah
-        //CONFIG3 atau “low_fee”. Akan tetapi jika lebih dari “threshold”, maka biaya
-        //transfer diambil dari nilai CONFIG4 atau “high_fee”.
-        //2. Total biaya yang perlu dibayarkan adalah hasil penjumlahan dari jumlah uang
-        //yang akan ditransfer dan biaya transfer.
-        //3. Pesan output apabila CONFIG1 atau “lang” bernilai “en” adalah “Transfer fee =
-        //< biaya_transfer >” dan “Total amount = < nominal_transfer + biaya_transfer >.
-        //4. Pesan output apabila CONFIG1 atau “lang” bernilai “id” adalah “Biaya transfer =
-        //< biaya_transfer >” dan “Total biaya = < nominal_transfer + biaya_transfer >.
-
         public Config config { get; set; }
         private string filepath = "bank_transfer_config.json";
 
