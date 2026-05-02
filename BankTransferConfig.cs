daaa02351686f66f94d99e778e3e27aed78e5c11
MoCheeseKy <rifky.muhammadprayudhi@gmail.com>
2026-05-02 15:07:48 +0700

    second commit

---
+++ BankTransferConfig.cs
@@ -0,0 +1,85 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+using System.IO;
+
+namespace Modul9_103022400003
+{
+    public class BankTransferConfig
+    {
+        
+
+        //File json
+        //{
+        //  "lang": "CONFIG1",
+        //  "transfer": {
+        //    "threshold": "CONFIG2",
+        //    "low_fee": "CONFIG3",
+        //    "high_fee": "CONFIG4"
+        //  },
+        //  "methods": "CONFIG5",
+        //  "confirmation": {
+        //    "en": "CONFIG6",
+        //    "id": "CONFIG7"
+        //  }
+        //}
+
+        //1. bila jumlah yang yang di-transfer pada bagian sebelumnya(4-a) kurang dari
+        //atau sama dengan nilai CONFIG2 atau “threshold”, maka biaya transfer adalah
+        //CONFIG3 atau “low_fee”. Akan tetapi jika lebih dari “threshold”, maka biaya
+        //transfer diambil dari nilai CONFIG4 atau “high_fee”.
+        //2. Total biaya yang perlu dibayarkan adalah hasil penjumlahan dari jumlah uang
+        //yang akan ditransfer dan biaya transfer.
+        //3. Pesan output apabila CONFIG1 atau “lang” bernilai “en” adalah “Transfer fee =
+        //< biaya_transfer >” dan “Total amount = < nominal_transfer + biaya_transfer >.
+        //4. Pesan output apabila CONFIG1 atau “lang” bernilai “id” adalah “Biaya transfer =
+        //< biaya_transfer >” dan “Total biaya = < nominal_transfer + biaya_transfer >.
+
+        public Config config { get; set; }
+        private string filepath = "bank_transfer_config.json";
+
+        public BankTransferConfig() {
+            try
+            {
+                ReadConfigFile();
+                Console.WriteLine("Sukses Bro!");
+            }
+            catch
+            {
+                SetDefault();
+                WriteConfigFile();
+            }
+        }
+        public void ReadConfigFile()
+        {
+            string jsonString = System.IO.File.ReadAllText(filepath);
+            config = System.Text.Json.JsonSerializer.Deserialize<Config>(jsonString);
+        }
+
+        public void WriteConfigFile()
+        {
+            string jsonString = System.Text.Json.JsonSerializer.Serialize(config);
+            System.IO.File.WriteAllText(filepath, jsonString);
+        }
+
+        public void SetDefault()
+        {
+           config = new Config
+            {
+                lang = "en",
+                transfer = new Transfer
+                {
+                    treshold = 25000000,
+                    low_fee = 6500,
+                    high_fee = 15000
+                },
+                methods = new List<string> { "RTO (real-time)", "SKN", "RTGS", "BI FAST" },
+                confirmation = new Confirmation
+                {
+                    en = "yes",
+                    id = "ya"
+                }
+            };
+        }
+    }
+}
--- Program.cs
+++ Program.cs
@@ -1,2 +1,47 @@
 ﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+
+using Modul9_103022400003;
+
+class Program
+{
+    static void Main(string[] args)
+    {
+        BankTransferConfig bankTransferConfig = new BankTransferConfig();
+        double amount;
+
+        if (bankTransferConfig.config.lang == "en")
+        {
+            Console.WriteLine("Please insert the amount of money to transfer:");
+            amount = int.Parse(Console.ReadLine());
+            Console.WriteLine("Transfer Fee: " +
+                (amount <= bankTransferConfig.config.transfer.treshold
+                    ? bankTransferConfig.config.transfer.low_fee
+                    : bankTransferConfig.config.transfer.high_fee)
+                );
+            Console.WriteLine("Amount: " + (amount + (amount <= bankTransferConfig.config.transfer.treshold
+                     ? bankTransferConfig.config.transfer.low_fee
+                     : bankTransferConfig.config.transfer.high_fee)
+                 ));
+            Console.WriteLine("Select transfer method: ");
+            bankTransferConfig.config.methods.ForEach(method => Console.WriteLine(method));
+        }
+        else if (bankTransferConfig.config.lang == "id")
+        {
+            Console.WriteLine("Masukkan jumlah uang yang akan di transfer:");
+            amount = int.Parse(Console.ReadLine());
+            Console.WriteLine("Biaya Transfer: " +
+                 (amount <= bankTransferConfig.config.transfer.treshold
+                     ? bankTransferConfig.config.transfer.low_fee
+                     : bankTransferConfig.config.transfer.high_fee)
+                 );
+            Console.WriteLine("Total Biaya: " + (amount + (amount <= bankTransferConfig.config.transfer.treshold
+                     ? bankTransferConfig.config.transfer.low_fee
+                     : bankTransferConfig.config.transfer.high_fee)
+                 ));
+            Console.WriteLine("Pilih metode transfer: ");
+            bankTransferConfig.config.methods.ForEach(method => Console.WriteLine(method));
+        }
+    }
+}
+
+
